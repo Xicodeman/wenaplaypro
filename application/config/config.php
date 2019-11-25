b8b69ca3c3b1dabf8277e22f3cc381e54bba78a6
Heritier <hmuhire15@alustudent.com>
2019-11-25 11:36:00 +0400

    updates

--- application/config/config.php
+++ application/config/config.php
@@ -38,7 +38,8 @@ $config['base_url'] = $root;
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php?';
+/*$config['index_page'] = 'index.php?'; */
+$config['index_page'] = "index.php?";
 
 /*
 |--------------------------------------------------------------------------
